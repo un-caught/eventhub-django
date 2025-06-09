eb3aadd386307fe52bc492e99df69dc2bca16d04
un-caught <korodeleboluwatife@gmail.com>
2025-06-09 04:59:52 -0700

    Add files via upload

---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    saved_events = models.ManyToManyField('events.Event', blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics', default='default.jpg')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
